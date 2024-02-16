18677104de24aa96009d98f22190419571c6a7cd
Elvis <elvismomanyi23@gmail.com>
2024-02-16 15:07:20 +0300

    working on improving the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-export default function Home(){
+import React, { useEffect, useState } from 'react';
+import '../styles.css'; 
+
+export default function Home() {
+    const [showAlert, setShowAlert] = useState(true);
+
+    useEffect(() => {
+        if (showAlert) {
+            alert('Welcome to ticketsasa.com!');
+            setShowAlert(false); 
+        }
+    }, [showAlert]); 
+
     return (
-        <>
-            <div>
-                Home 
-            </div>
-        </>
-    )
+        <div className="container">
+            <h1 className="title">Welcome to ticketsasa</h1>
+            <p>Exciting Events Await! Seize the Moment and Grab Your Tickets Today! Don't Miss Out on the Experience. Act Fast While Tickets Are Still Available. Join Us for Unforgettable Moments and Create Memories That Last a Lifetime.</p>
+        </div>
+    );
 }
+
